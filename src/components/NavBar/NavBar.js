6f03b2ba6e3e37016ec0d225e29145954a203106
Raiannbr <braian.link@gmail.com>
2021-09-01 18:24:33 -0300

    segunda

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = ()=>{
+    return(
+        <nav className="navbar navbar-light bg-dark" >
+        <form className="container-fluid justify-content-start">
+          <button className="btn btn-outline-success me-2" type="button">Alianza Reverde</button>
+          <button className="btn btn-sm btn-outline-secondary mx-auto" type="button">Productos</button>
+          <button className="btn btn-sm btn-outline-secondary mx-auto" type="button">Nosotros</button>
+          <img src="../src/img/34627.png"></img>
+        </form>
+        </nav>
+    )
+}
+
+export default NavBar
